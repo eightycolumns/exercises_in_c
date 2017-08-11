e6dbab0e476578b5c443ec23b2eb0d29b0c65495
Drew Keller <drew@eightycolumns.com>
2017-08-10 20:45:41 -0400

    Simplify conditionals with ternary operator

--- checkerboard.c
+++ checkerboard.c
@@ -6,11 +6,7 @@ bool is_even(int d);
 int main(void) {
   for (int i = 0; i < 8; i += 1) {
     for (int j = 0; j < 8; j += 1) {
-      if (is_even(i + j)) {
-        printf("*");
-      } else {
-        printf(" ");
-      }
+      printf(is_even(i + j) ? "*" : " ");
     }
 
     printf("\n");
