eda45c39d1d173bdd89d65b38b94b752f5f67deb
Drew Keller <drew@eightycolumns.com>
2017-08-14 21:55:26 -0400

    Extract integer_to_binary_string()

--- convert_integer_to_binary_string.c
+++ convert_integer_to_binary_string.c
@@ -3,24 +3,29 @@
 
 #define BINARY_STRING_MAX_LENGTH 8
 
+char *integer_to_binary_string(char *binary_string, int d);
+
 int main(void) {
   for (int i = 0; i < 256; i += 1) {
     char binary_string[BINARY_STRING_MAX_LENGTH + 1];
-
-    if (i == 0) {
-      strcpy(binary_string, "0");
-    } else {
-      strcpy(binary_string, "");
-
-      for (int j = i; j > 0; j /= 2) {
-        char temp[strlen(binary_string) + 1];
-        strcpy(temp, binary_string);
-
-        sprintf(binary_string, "%d", j % 2);
-        strcat(binary_string, temp);
-      }
-    }
-
-    printf("%3d\t%8s\n", i, binary_string);
+    printf("%3d\t%8s\n", i, integer_to_binary_string(binary_string, i));
   }
+}
+
+char *integer_to_binary_string(char *binary_string, int d) {
+  if (d == 0) {
+    strcpy(binary_string, "0");
+  } else {
+    strcpy(binary_string, "");
+
+    for (int i = d; i > 0; i /= 2) {
+      char temp[strlen(binary_string) + 1];
+      strcpy(temp, binary_string);
+
+      sprintf(binary_string, "%d", i % 2);
+      strcat(binary_string, temp);
+    }
+  }
+
+  return binary_string;
 }
